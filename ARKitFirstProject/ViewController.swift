6dcf33a400c12b5a1fadba552f401ef74849fe27
Mark Santoro <mark_santoro@comcast.net>
2024-08-23 08:44:35 -0700

    initial setup

--- ARKitFirstProject/ViewController.swift
+++ ARKitFirstProject/ViewController.swift
@@ -23,10 +23,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let shipScene = SCNScene(named: "art.scnassets/ship.scn")!
         
         // Set the scene to the view
-        sceneView.scene = scene
+        sceneView.scene = shipScene
     }
     
     override func viewWillAppear(_ animated: Bool) {
