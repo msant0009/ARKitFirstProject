03b9af52fe9f0dfd5d233b6445d23c4fa8a111f1
Mark Santoro <mark_santoro@comcast.net>
2024-08-23 09:07:07 -0700

    add motion to basic ship scene

--- ARKitFirstProject/ViewController.swift
+++ ARKitFirstProject/ViewController.swift
@@ -27,6 +27,21 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the scene to the view
         sceneView.scene = shipScene
+        
+        for node in sceneView.scene.rootNode.childNodes {
+            
+            
+            let moveShip = SCNAction.moveBy(x: 1.0, y: 0.5, z: -0.5, duration: 1)
+            let fadeOut = SCNAction.fadeOpacity(to: 0.5, duration: 1)
+            let fadeIn = SCNAction.fadeOpacity(to: 1, duration: 1)
+            let sequence = SCNAction.sequence([moveShip,fadeOut,fadeIn])
+            
+            let repeatForever = SCNAction.repeatForever(sequence)
+            
+            node.runAction(repeatForever)
+            
+        }
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
